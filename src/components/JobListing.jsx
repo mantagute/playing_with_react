8e84b2b986f7d45d4237e50e425032cc87b5e304
Mantagute <j285570@dac.unicamp.br>
2025-10-20 20:55:15 -0300

    layouts + router provider

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {FaMapMarker} from 'react-icons/fa';
 
 function JobListing({ job }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -30,7 +31,7 @@ function JobListing({ job }) {
 
       <div className="flex flex-col lg:flex-row justify-between mb-4">
         <div className="text-orange-700 mb-3">
-          <i className="fa-solid fa-location-dot text-lg"></i> {job.location}
+          <FaMapMarker className='inline text-lg mb-1 mr-1'/>{job.location}
         </div>
         <a
           href={`/job/${job.id}`}
